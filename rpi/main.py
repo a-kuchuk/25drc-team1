ddeb753b1ccba3b9d762eb0e3f2eafbeb69ffc13
Andrea Kuchuk <kuchukandrea@gmail.com>
2025-07-02 14:02:35 +1000

    s

--- rpi/main.py
+++ rpi/main.py
@@ -41,7 +41,7 @@ arrow_cooldown = 0
 def drive(steering_angle=0, speed=BASE_SPEED, timeout=0.05):
     steering.set_steering_angle(steering_angle-5)
     motor.forward(speed)
-    time.sleep(timeout)
+    # time.sleep(timeout)
     return
 
 def main_loop():
