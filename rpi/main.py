8a135e3f8e39c8e8ee0a2e1922b932062bddf8a1
Andrea Kuchuk <kuchukandrea@gmail.com>
2025-07-03 15:13:22 +1000

    s

--- rpi/main.py
+++ rpi/main.py
@@ -170,11 +170,11 @@ def main_loop():
         if obj_x < FRAME_WIDTH // 2:
             print("Object on left — turning right to avoid")
             # time.sleep(0.2)
-            drive(35, BASE_SPEED, 0.2)
+            drive(30, BASE_SPEED, 0.2)
         else:
             print("Object on right — turning left to avoid")
             # time.sleep(0.2)
-            drive(-35, BASE_SPEED, 0.2)
+            drive(-30, BASE_SPEED, 0.2)
         return
 
 
